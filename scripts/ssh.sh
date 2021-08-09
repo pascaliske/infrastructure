a70e3cdb07c9b8eb80effa0cd36aa1b09c7a6787
Pascal Iske <info@pascaliske.dev>
2021-08-09 09:25:15 +0200

    refactor: use inventory host names for role exceptions

---
+++ scripts/ssh.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+# fetch connection details
+HOST=$(ansible-inventory --host "$1" | jq -r '.ansible_host')
+USER=$(ansible-inventory --host "$1" | jq -r '.ansible_user')
+
+# enter ssh session
+ssh -t -i "$ANSIBLE_PRIVATE_KEY_FILE" "$USER@$HOST"
