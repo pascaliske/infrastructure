5d25b1b2c187ddf9d69568382add090836d5b541
Pascal Iske <info@pascaliske.dev>
2022-11-07 09:25:26 +0100

    chore(github): implement workflow for terraform

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,14 +1,10 @@
-data "http" "ipv4" {
-  url = "https://ifconfig.co"
-}
-
 # zones
 data "cloudflare_zone" "zone_private" {
-  name = var.domain_private
+  name = local.domain_private
 }
 
 data "cloudflare_zone" "zone_public" {
-  name = var.domain_public
+  name = local.domain_public
 }
 
 # vpn, will be updated frequently using a K8s CronJob
@@ -16,61 +12,66 @@ resource "cloudflare_record" "vpn" {
   zone_id = data.cloudflare_zone.zone_private.id
   type    = "A"
   name    = "vpn"
-  value   = trimspace(data.http.ipv4.response_body)
+  value   = ""
+  proxied = true
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
 
 # dmarc
 resource "cloudflare_record" "dmarc" {
   zone_id = data.cloudflare_zone.zone_private.id
-  type = "TXT"
-  name = "_dmarc"
-  value = "v=DMARC1; p=quarantine; rua=mailto:info@pascaliske.dev"
+  type    = "TXT"
+  name    = "_dmarc"
+  value   = "v=DMARC1; p=quarantine; rua=mailto:info@${local.domain_public}"
 }
 
 # dkim
 resource "cloudflare_record" "dkim" {
   zone_id = data.cloudflare_zone.zone_private.id
-  type = "TXT"
-  name = "*._domainkey"
-  value = "v=DKIM1; p="
+  type    = "TXT"
+  name    = "*._domainkey"
+  value   = "v=DKIM1; p="
 }
 
 # spf
 resource "cloudflare_record" "spf" {
   zone_id = data.cloudflare_zone.zone_private.id
-  type = "TXT"
-  name = var.domain_private
-  value = "v=spf1 ~all"
+  type    = "TXT"
+  name    = local.domain_private
+  value   = "v=spf1 ~all"
 }
 
 # google
 resource "cloudflare_record" "google" {
   zone_id = data.cloudflare_zone.zone_private.id
-  type = "TXT"
-  name = var.domain_private
-  value = "google-site-verification=${var.google_verification_token}"
+  type    = "TXT"
+  name    = local.domain_private
+  value   = "google-site-verification=${var.GOOGLE_VERIFICATION_TOKEN}"
 }
 
 # public
 resource "cloudflare_record" "public" {
   zone_id = data.cloudflare_zone.zone_public.id
-  type = "A"
-  name = "*"
-  value = var.public_ip_jakku
+  type    = "A"
+  name    = "*"
+  value   = local.public_ip_jakku
 }
 
 # docs
 resource "cloudflare_record" "docs" {
   zone_id = data.cloudflare_zone.zone_public.id
-  type = "CNAME"
-  name = "k8s"
-  value = "pascaliske.github.io"
+  type    = "CNAME"
+  name    = "k8s"
+  value   = local.domain_github
 }
 
 # charts
 resource "cloudflare_record" "charts" {
   zone_id = data.cloudflare_zone.zone_public.id
-  type = "CNAME"
-  name = "charts"
-  value = "pascaliske.github.io"
+  type    = "CNAME"
+  name    = "charts"
+  value   = local.domain_github
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,3 @@
-variable "domain_private" {
-  type = string
-}
-
-variable "domain_public" {
-  type = string
-}
-
-variable "public_ip_jakku" {
-  type = string
-}
-
-variable "google_verification_token" {
+variable "GOOGLE_VERIFICATION_TOKEN" {
   type = string
 }
