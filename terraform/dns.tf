35b7da94b70582570973d03469337856992aa43a
Pascal Iske <info@pascaliske.dev>
2024-08-13 04:31:17 +0000

    chore(deps): update terraform cloudflare to v4.39.0

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -3,11 +3,11 @@ resource "cloudflare_record" "vpn" {
   zone_id = data.cloudflare_zone.zone_internal.id
   type    = "A"
   name    = "vpn"
-  value   = ""
+  content = ""
   proxied = false
 
   lifecycle {
-    ignore_changes = [value]
+    ignore_changes = [content]
   }
 }
 
@@ -16,7 +16,7 @@ resource "cloudflare_record" "dmarc" {
   zone_id = data.cloudflare_zone.zone_internal.id
   type    = "TXT"
   name    = "_dmarc"
-  value   = "v=DMARC1; p=quarantine; rua=mailto:info@${local.domain_external}"
+  content = "v=DMARC1; p=quarantine; rua=mailto:info@${local.domain_external}"
 }
 
 # dkim
@@ -24,7 +24,7 @@ resource "cloudflare_record" "dkim" {
   zone_id = data.cloudflare_zone.zone_internal.id
   type    = "TXT"
   name    = "*._domainkey"
-  value   = "v=DKIM1; p="
+  content = "v=DKIM1; p="
 }
 
 # spf
@@ -32,7 +32,7 @@ resource "cloudflare_record" "spf" {
   zone_id = data.cloudflare_zone.zone_internal.id
   type    = "TXT"
   name    = local.domain_internal
-  value   = "v=spf1 ~all"
+  content = "v=spf1 ~all"
 }
 
 # google
@@ -40,7 +40,7 @@ resource "cloudflare_record" "google" {
   zone_id = data.cloudflare_zone.zone_internal.id
   type    = "TXT"
   name    = local.domain_internal
-  value   = "google-site-verification=${var.GOOGLE_VERIFICATION_TOKEN}"
+  content = "google-site-verification=${var.GOOGLE_VERIFICATION_TOKEN}"
 }
 
 # wildcard
@@ -48,7 +48,7 @@ resource "cloudflare_record" "wildcard" {
   zone_id = data.cloudflare_zone.zone_external.id
   type    = "A"
   name    = "*"
-  value   = local.public_ip_jakku
+  content = local.public_ip_jakku
 }
 
 # docs
@@ -56,7 +56,7 @@ resource "cloudflare_record" "docs" {
   zone_id = data.cloudflare_zone.zone_external.id
   type    = "CNAME"
   name    = "k8s"
-  value   = local.domain_github
+  content = local.domain_github
 }
 
 # charts
@@ -64,5 +64,5 @@ resource "cloudflare_record" "charts" {
   zone_id = data.cloudflare_zone.zone_external.id
   type    = "CNAME"
   name    = "charts"
-  value   = local.domain_github
+  content = local.domain_github
 }
