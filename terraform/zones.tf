37d275f25047372ff562aea72665fc1e0ea068d1
Pascal Iske <info@pascaliske.dev>
2025-02-13 21:55:00 +0100

    chore(deps): update terraform cloudflare to v5

--- terraform/zones.tf
+++ terraform/zones.tf
@@ -1,9 +1,17 @@
 # internal / private
-data "cloudflare_zone" "zone_internal" {
+data "cloudflare_zones" "zone_search_internal" {
   name = local.domain_internal
 }
 
+data "cloudflare_zone" "zone_internal" {
+  zone_id = data.cloudflare_zones.zone_search_internal.result[0].id
+}
+
 # external / public
-data "cloudflare_zone" "zone_external" {
+data "cloudflare_zones" "zone_search_external" {
   name = local.domain_external
+}
+
+data "cloudflare_zone" "zone_external" {
+  zone_id = data.cloudflare_zones.zone_search_external.result[0].id
 }
