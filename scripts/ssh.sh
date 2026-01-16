055a26365a5eec43d62437eac8c0f6610c6ac13d
Pascal Iske <info@pascaliske.dev>
2026-01-16 09:14:15 +0100

    refactor: remove legacy ssh script

--- scripts/ssh.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-# fetch connection user
-USER=$(ansible-inventory -i ansible/inventory --host "$1" | jq -r '.ansible_user')
-
-# enter ssh session
-ssh -t -i "$ANSIBLE_PRIVATE_KEY_FILE" "$USER@$1" "${@:2}"
